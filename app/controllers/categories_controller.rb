63295f7dc2a5a0644cbf5e33d55c166ac01b8e91
Muhammad Abubakar <muhammadabubakar0419@gmail.com>
2017-10-10 14:10:29 -0400

    category is working...

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,57 @@
 class CategoriesController < ApplicationController
+    before_action :set_category, only: [:show, :edit, :update, :destroy]
+
     def index
-        @categories = Category.order('id ASC').all
+        @user = User.find(params[:user_id])
+        @categories = Todolist.pluck(:category)
+
     end
 
     def show
-        @category = Category.find(params[:id])
-        # @comments = Comment.where("user_id = ?", params[:id])
+
     end
+
+    def destroy
+        @category = Todolist.pluck(:category)
+        @category.destroy
+        respond_to do |format|
+          format.html { redirect_to user_categories_path, notice: 'Post was successfully destroyed.' }
+          format.json { head :no_content }
+        end
+    end
+
+    def update
+        # p = todolist_params
+        # p[:category] = Category.find(todolist_params[:category])
+        @category = Todolist.pluck(:category)
+           respond_to do |format|
+             if @category.update(todolist_params)
+               format.html { redirect_to user_categories_path, notice: 'Post was successfully updated.' }
+             else
+               format.html { render :edit }
+             end
+           end
+       end
+
+       private
+       def set_category 
+        @user = User.find(params[:user_id])
+        @category = Todolist.where(category:(params[:id]))
+        
+        
+    
+        
+       end
+        def todolist_params
+         params.require(:category).permit(:name)
+       end
+
 end
+
+
+
+
+  
+
+
+   
